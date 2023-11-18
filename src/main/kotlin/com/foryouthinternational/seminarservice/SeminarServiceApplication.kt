663d75b1baa3692b21f53236cafbde63d40786c4
Alfian Tamim Andhika <alfiantamimandhika@gmail.com>
2023-11-18 20:13:59 +0700

    Initial commit

---
+++ src/main/kotlin/com/foryouthinternational/seminarservice/SeminarServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.foryouthinternational.seminarservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeminarServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<SeminarServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/foryouthinternational/seminarservice/SeminarServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.foryouthinternational.seminarservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SeminarServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
